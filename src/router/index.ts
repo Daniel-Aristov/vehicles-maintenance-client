f807be7e419ad29531987cfb7053b31a2a10b617
Даниил Аристов <danilka-aristov@mail.ru>
2025-05-23 10:29:12 +0300

    По умолчанию в приложении редиректить в "гараж"

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,7 +9,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 const routes = [
   {
     path: '/',
-    redirect: '/profile'
+    redirect: '/vehicles'
   },
   {
     path: '/login',
